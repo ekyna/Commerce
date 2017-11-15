777844bfef34e12cbc604f223a1a83519fcd6da7
Etienne Dauvergne <contact@ekyna.com>
2017-11-15 15:51:27 +0100

    [Commerce] Fixed outstanding payment releaser.

--- Payment/Releaser/OutstandingReleaser.php
+++ Payment/Releaser/OutstandingReleaser.php
@@ -3,6 +3,7 @@
 namespace Ekyna\Component\Commerce\Payment\Releaser;
 
 use Ekyna\Component\Commerce\Common\Model\SaleInterface;
+use Ekyna\Component\Commerce\Common\Util\Money;
 use Ekyna\Component\Commerce\Payment\Calculator\PaymentCalculatorInterface;
 use Ekyna\Component\Commerce\Payment\Model\PaymentStates;
 use Ekyna\Component\Resource\Persistence\PersistenceHelperInterface;
@@ -44,16 +45,25 @@ class OutstandingReleaser implements ReleaserInterface
      */
     public function releaseFund(SaleInterface $sale)
     {
-        $overpaidAmount = $sale->getPaidTotal() + $sale->getOutstandingAccepted() - $sale->getGrandTotal();
+        $currency = $sale->getCurrency()->getCode();
+
+        $overpaidAmount =
+            $sale->getPaidTotal()
+            + $sale->getOutstandingExpired()
+            + $sale->getOutstandingAccepted()
+            - $sale->getGrandTotal();
+
+        $overpaidAmount = Money::round($overpaidAmount, $currency);
 
         // Abort if the sale is not overpaid
         if (0 >= $overpaidAmount) {
             return false;
         }
 
-        $currency = $sale->getCurrency()->getCode();
-
         $changed = false;
+
+        $paidStates = PaymentStates::getPaidStates();
+        $paidStates[] = PaymentStates::STATE_EXPIRED;
 
         // For each payments
         foreach ($sale->getPayments() as $payment) {
@@ -63,20 +73,21 @@ class OutstandingReleaser implements ReleaserInterface
             }
 
             // Continue if the payment is not paid
-            if (!PaymentStates::isPaidState($payment->getState())) {
+            if (!in_array($payment->getState(), $paidStates, true)) {
                 continue;
             }
 
             // If the payment amount is less than or equal the overpaid amount
             $amount = $this->paymentCalculator->convertPaymentAmount($payment, $currency);
-            if ($amount <= $overpaidAmount) {
+            if (0 <= Money::compare($overpaidAmount, $amount, $currency)) {
                 // Cancel the payment
                 $payment->setState(PaymentStates::STATE_CANCELED);
 
                 $this->persistenceHelper->persistAndRecompute($payment, true);
 
                 $changed = true;
-                $overpaidAmount -= $payment->getAmount();
+
+                $overpaidAmount = Money::round($overpaidAmount - $amount, $currency);
 
                 // Break if the sale is no no longer overpaid
                 if (0 >= $overpaidAmount) {
